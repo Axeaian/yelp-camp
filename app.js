c104588aaf140febc3564b16ae7209a4f84d919b
xfc <gnafnix@gmail.com>
2018-08-06 07:19:01 +0000

    add mongolab

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var campgroundRoutes = require("./routes/campgrounds"),
     authRoutes       = require("./routes/auth");
     
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true });
+mongoose.connect("mongodb://gnyx89:gohahaha89@ds255258.mlab.com:55258/yelpcamp",{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
